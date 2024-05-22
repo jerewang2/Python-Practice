2a390b368e07e1d2a6356f619db9b6a273735346
Jeremy Wang <jwang3320@gmail.com>
2024-05-21 23:14:18 -0400

    Brute Force Two Sum.

--- blind_75/1_Two_Sum.py
+++ blind_75/1_Two_Sum.py
@@ -17,4 +17,21 @@
 
 # Follow-up: Can you come up with an algorithm that is less than O(n^2) time complexity?
 
-nums = [2, 7, 11, 15]
+def two_sums(nums, target):
+    if len(nums) == 0:
+        return []
+
+    for ind, num in enumerate(nums):
+        for next_ind, new_num in enumerate(nums):
+            if ind == next_ind:
+                continue
+            elif (num + new_num) == target:
+                return [ind, next_ind]
+
+questions = ([2, 7, 11, 15], [3, 2, 4], [3, 3])
+targets = (9, 6, 6)
+answers = ([0, 1], [1, 2], [0, 1])
+
+for i in range(len(questions)):
+    print(f'Question {i + 1}: {questions[i]} | Answer: {answers[i]}')
+    print(f'My Answer: {two_sums(questions[i], targets[i])}\n')
