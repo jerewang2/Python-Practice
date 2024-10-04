aa195f51a521af14a9fdb40b7eafb23e1a83d74d
Jeremy Wang <jwang3320@gmail.com>
2024-10-04 16:07:25 -0400

    Update Singly and Doubly Linked Lists

--- Algorithms/Linked_Lists/doubly_linked_lists.py
+++ Algorithms/Linked_Lists/doubly_linked_lists.py
@@ -1,14 +1,18 @@
 class DoublyLinkedLists(object):
 
-    def __init__(self, value):
+    def __init__(self, value, next_node = None, prev_node = None):
         self.value = value
-        self.next_node = None
-        self.prev_node = None
+        self.next_node = next_node
+        self.prev_node = prev_node
+    
+    def __str__(self):
+        return str(self.value)
 
-def main():
-    a = DoublyLinkedLists(1)
-    b = DoublyLinkedLists(2)
-    c = DoublyLinkedLists(3)
+def main_1():
+    head = tail = DoublyLinkedLists(1)
+    a = DoublyLinkedLists(2)
+    b = DoublyLinkedLists(3)
+    c = DoublyLinkedLists(4)
 
     a.next_node = b
     b.prev_node = a
@@ -19,6 +23,47 @@ def main():
     print(f'Node a: {a.value} | Next Node a: {a.next_node.value}')
     print(f'Node b: {b.value} | Prev Node b: {b.prev_node.value} | Next Node b: {b.next_node.value}')
     print(f'Node c: {c.value} | Prev Node c: {c.prev_node.value}')
+
+    
+class DoublyNode:
+    def __init__(self, val, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return str(self.val)
+
+def main():
+    head = tail = DoublyNode(1)
+    print(tail)
+
+    def display(head):
+        curr = head
+        elements = []
+        while curr:
+            elements.append(str(curr.val))
+            curr = curr.next
+        print(' <-> '.join(elements))
+    
+    display(head)
+
+    def insert_at_beginning(head, tail, val):
+        new_node = DoublyNode(val, next = head)
+        head.prev = new_node
+        return new_node, tail
+
+    def insert_at_end(head, tail, val):
+        new_node = DoublyNode(val, prev = tail)
+        tail.next = new_node
+        return head, new_node
+
+    head, tail = insert_at_beginning(head, tail, 3)
+    display(head)
+
+    head, tail = insert_at_end(head, tail, 7)
+    display(head)
+
 
 if __name__ == '__main__':
     main()
--- Algorithms/Linked_Lists/linked_lists.py
+++ Algorithms/Linked_Lists/linked_lists.py
@@ -1,22 +1,58 @@
 class Node(object):
 
-    def __init__(self, value):
+    def __init__(self, value, next = None):
         self.value = value
-        self.nextnode = None
+        self.next = None
+    
+    def __str__(self):
+        return str(self.value)
 
 def main():
-    a = Node(1)
-    b = Node(2)
-    c = Node(3)
+    # Creating a linked list
+    Head = Node(1)
+    a = Node(2)
+    b = Node(3)
+    c = Node(7)
 
-    a.nextnode = b
-    b.nextnode = c
+    Head.next = a
+    a.next = b
+    b.next = c
 
+    print(f'Node head value: {Head.value}')
+    print(f'Next node of head: {Head.next.value}')
     print(f'Node a value: {a.value}')
-    print(f'Next node of a: {a.nextnode.value}')
+    print(f'Next node of a: {a.next.value}')
     print(f'Node b value: {b.value}')
-    print(f'Next node of b: {b.nextnode.value}')
+    print(f'Next node of b: {b.next.value}')
     print(f'Node c value: {c.value}')
+
+    # Traversing a linked list O(n) time complexity
+    curr = Head
+    while curr:
+        print(curr)
+        curr = curr.next
+
+    # Display linked list O(n) time complexity
+    def display(head):
+        curr = head
+        elements = []
+        while curr:
+            elements.append(str(curr.value))
+            curr = curr.next
+        print(' -> '.join(elements))
+    
+    # Search for node value O(n) time complexity
+    def search(head, val):
+        curr = head
+        while curr:
+            if val == curr.value:
+                return True
+            else:
+                curr = curr.next
+        return False
+
+    display(Head)
+    print(search(Head, 7))
 
 if __name__ == "__main__":
     main()
